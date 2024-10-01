a9c637ca9c366840dd1ac121c085408871720ba2
Alberto Roman <romanvieito@gmail.com>
2024-10-01 14:53:51 -0400

    prevent text selection

--- app/components/PoseCanvas.js
+++ app/components/PoseCanvas.js
@@ -192,11 +192,21 @@ const PoseCanvas = ({ videoRef, poseLandmarker, videoDimensions, setFeedback, fe
             fontWeight: 'bold',
             cursor: 'pointer',
             touchAction: 'manipulation',
-            userSelect: 'none',
             WebkitTapHighlightColor: 'transparent',
+            WebkitUserSelect: 'none',  // For Safari
+            MsUserSelect: 'none',      // For Internet Explorer/Edge
+            userSelect: 'none',        // Standard
           }}
         >
-          {document.fullscreenElement ? 'Exit Full' : 'Full Screen'}
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+            {document.fullscreenElement ? (
+              // Exit fullscreen icon
+              <path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/>
+            ) : (
+              // Enter fullscreen icon
+              <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
+            )}
+          </svg>
         </button>
       </div>
     </div>
