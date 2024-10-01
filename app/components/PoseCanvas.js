478b321293bfeea50dd0eb69155b8b13d4334c42
Alberto Roman <romanvieito@gmail.com>
2024-10-01 11:28:12 -0400

    full screen control

--- app/components/PoseCanvas.js
+++ app/components/PoseCanvas.js
@@ -60,6 +60,18 @@ const PoseCanvas = ({ videoRef, poseLandmarker, videoDimensions, setFeedback, fe
     // The Overlay component will handle the timing now
   }, [setFeedback]);
 
+  const handleFullScreen = () => {
+    if (canvasRef.current.requestFullscreen) {
+      canvasRef.current.requestFullscreen();
+    } else if (canvasRef.current.mozRequestFullScreen) { // Firefox
+      canvasRef.current.mozRequestFullScreen();
+    } else if (canvasRef.current.webkitRequestFullscreen) { // Chrome, Safari and Opera
+      canvasRef.current.webkitRequestFullscreen();
+    } else if (canvasRef.current.msRequestFullscreen) { // IE/Edge
+      canvasRef.current.msRequestFullscreen();
+    }
+  };
+
   useEffect(() => {
     let animationId;
     const canvasElement = canvasRef.current;
@@ -160,6 +172,13 @@ const PoseCanvas = ({ videoRef, poseLandmarker, videoDimensions, setFeedback, fe
     <div style={{ position: 'relative' }}>
       <canvas ref={canvasRef} style={{ width: `${videoDimensions.width}px`, height: `${videoDimensions.height}px` }}></canvas>
       <Overlay statistics={feedback ? [feedback] : []} visible={true} />
+      <div style={{ position: 'absolute', bottom: 10, right: 10, color: 'white', cursor: 'pointer' }}>
+        <button onClick={handleFullScreen}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
+            <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
