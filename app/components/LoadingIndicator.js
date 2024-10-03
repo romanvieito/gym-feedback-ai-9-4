ade2d0f838bf13d1b07689ee88d7ec4db66a510a
Alberto Roman <romanvieito@gmail.com>
2024-10-03 19:19:58 -0400

    having fun loading

--- app/components/LoadingIndicator.js
+++ app/components/LoadingIndicator.js
@@ -2,18 +2,18 @@ import React from 'react';
 import LinearProgressWithLabel from './LinearProgressWithLabel';
 
 const LoadingIndicator = ({ progress }) => (
-  <>
+  <div style={{ color: 'white' }}>
     {progress > 0 && progress < 100 ? (
       <>
-        <p>Downloading model to process poses ... </p>
-        <LinearProgressWithLabel value={progress} />
+        <p>Downloading dance moves... Get ready to boogie! 🕺💃</p>
+        <LinearProgressWithLabel value={progress} color="white" />
       </>
     ) : progress === 100 ? (
-      <p>Downloaded model</p>
+      <p>Woohoo! Dance floor is ready! 🎉</p>
     ) : (
-      <p>Getting started</p>
+      <p>Warming up the disco ball... 🪩</p>
     )}
-  </>
+  </div>
 );
 
 export default LoadingIndicator;
