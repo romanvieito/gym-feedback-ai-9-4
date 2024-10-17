aadf1374649e38b2f94513b40387d64faf47e33b
Abel Diaz Berenguer <abel.diaz.berenguer@gmail.com>
2024-10-17 12:46:13 +0200

    comenta Euclidean

--- app/components/PoseCanvas.js
+++ app/components/PoseCanvas.js
@@ -62,14 +62,14 @@ const PoseCanvas = forwardRef(({ videoRef, poseLandmarker, videoDimensions, setF
 
   const [poseMatchPercentage, setPoseMatchPercentage] = useState(100);
 
-  // Add euclideanDistance function
-  function euclideanDistance(point1, point2) {
-    return Math.sqrt(
-      Math.pow(point1.x - point2.x, 2) +
-      Math.pow(point1.y - point2.y, 2) +
-      Math.pow(point1.z - point2.z, 2)
-    );
-  }
+  // // Add euclideanDistance function
+  // function euclideanDistance(point1, point2) {
+  //   return Math.sqrt(
+  //     Math.pow(point1.x - point2.x, 2) +
+  //     Math.pow(point1.y - point2.y, 2) +
+  //     Math.pow(point1.z - point2.z, 2)
+  //   );
+  // }
 
   // Update getColorFromPercentage function
   function getColorFromPercentage(percentage) {
